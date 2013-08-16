c397914d347ca4a27a9a9cfc57cc3ad333aabe16
Peter Mitchell <peterjmit@gmail.com>
2013-08-16 17:42:41 -0400

    Stubbed templating

--- spec/Peterjmit/BlogBundle/Controller/BlogControllerSpec.php
+++ spec/Peterjmit/BlogBundle/Controller/BlogControllerSpec.php
@@ -11,16 +11,21 @@ use Doctrine\Common\Persistence\ManagerRegistry;
 use Doctrine\Common\Persistence\ObjectManager;
 use Doctrine\Common\Persistence\ObjectRepository;
 
+use Symfony\Bundle\FrameworkBundle\Templating\EngineInterface;
+use Symfony\Component\HttpFoundation\Response;
+
 class BlogControllerSpec extends ObjectBehavior
 {
     function let(
         ContainerInterface $container,
         ManagerRegistry $registry,
         ObjectManager $manager,
-        ObjectRepository $repository
+        ObjectRepository $repository,
+        EngineInterface $templating
     ) {
         $container->has('doctrine')->willReturn(true);
         $container->get('doctrine')->willReturn($registry);
+        $container->get('templating')->willReturn($templating);
 
         $registry->getManager()->willReturn($manager);
         $manager->getRepository('PeterjmitBlogBundle:Blog')->willReturn($repository);
@@ -33,9 +38,21 @@ class BlogControllerSpec extends ObjectBehavior
         $this->shouldHaveType('Peterjmit\BlogBundle\Controller\BlogController');
     }
 
-    function it_should_respond_to_index_action(ObjectRepository $repository)
-    {
+    function it_should_respond_to_index_action(
+        ObjectRepository $repository,
+        EngineInterface $templating,
+        Response $mockResponse
+    ) {
         $repository->findAll()->willReturn(array());
+
+        $templating
+            ->renderResponse(
+                'PeterjmitBlogBundle:Blog:index.html.twig',
+                array('posts' => array()),
+                null
+            )
+            ->willReturn($mockResponse)
+        ;
 
         $response = $this->indexAction();
 

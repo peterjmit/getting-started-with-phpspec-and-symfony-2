2a874803e1a1ee86ce6f6af8f60ca278a0069ce5
Peter Mitchell <peterjmit@gmail.com>
2013-08-16 16:49:27 -0400

    Fix controller not having access to container

--- spec/Peterjmit/BlogBundle/Controller/BlogControllerSpec.php
+++ spec/Peterjmit/BlogBundle/Controller/BlogControllerSpec.php
@@ -5,8 +5,15 @@ namespace spec\Peterjmit\BlogBundle\Controller;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
 class BlogControllerSpec extends ObjectBehavior
 {
+    function let(ContainerInterface $container)
+    {
+        $this->setContainer($container);
+    }
+
     function it_is_initializable()
     {
         $this->shouldHaveType('Peterjmit\BlogBundle\Controller\BlogController');

8ab17a28f3db6dffddffc5cb61a39bba6bd0801c
Peter Mitchell <peterjmit@gmail.com>
2013-08-16 17:21:48 -0400

    Stubbed doctrine

--- spec/Peterjmit/BlogBundle/Controller/BlogControllerSpec.php
+++ spec/Peterjmit/BlogBundle/Controller/BlogControllerSpec.php
@@ -7,10 +7,24 @@ use Prophecy\Argument;
 
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
+use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Common\Persistence\ObjectRepository;
+
 class BlogControllerSpec extends ObjectBehavior
 {
-    function let(ContainerInterface $container)
-    {
+    function let(
+        ContainerInterface $container,
+        ManagerRegistry $registry,
+        ObjectManager $manager,
+        ObjectRepository $repository
+    ) {
+        $container->has('doctrine')->willReturn(true);
+        $container->get('doctrine')->willReturn($registry);
+
+        $registry->getManager()->willReturn($manager);
+        $manager->getRepository('PeterjmitBlogBundle:Blog')->willReturn($repository);
+
         $this->setContainer($container);
     }
 
@@ -19,8 +33,10 @@ class BlogControllerSpec extends ObjectBehavior
         $this->shouldHaveType('Peterjmit\BlogBundle\Controller\BlogController');
     }
 
-    function it_should_respond_to_index_action()
+    function it_should_respond_to_index_action(ObjectRepository $repository)
     {
+        $repository->findAll()->willReturn(array());
+
         $response = $this->indexAction();
 
         $response->shouldHaveType('Symfony\Component\HttpFoundation\Response');
